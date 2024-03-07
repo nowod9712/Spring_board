72cef02ad98c57a2259050bd3fa7237c9524792a
nowod <ldowon1223@naver.com>
2024-03-07 19:29:17 +0900

    초기 환경 구축

---
+++ src/main/java/com/study/erum/HomeController.java
@@ -0,0 +1,15 @@
+package com.study.erum;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j
+public class HomeController {
+	@RequestMapping({"", "/"})
+	public String home() {
+		log.info("Welcome home!");
+		return "index";
+	}	
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>index</title>
+    <h2>Hello Springframework</h2>
+    <a href="/board/save">글작성</a>
+    <a href="/board">글목록</a>
+</head>
+<body>
+</body>
+</html>
