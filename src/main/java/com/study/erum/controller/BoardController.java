32f2f31f2a6d960b42afc38ab310aa3032ab9b31
nowod <ldowon1223@naver.com>
2024-03-07 19:41:36 +0900

    Path경로 설정오류 해결 및 글작성 기능 구현

---
+++ src/main/java/com/study/erum/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.study.erum.controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/save")
+    public String saveForm(){
+        return "save";
+    }
+}
